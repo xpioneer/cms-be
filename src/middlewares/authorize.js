1d7246a4b1f226b552185db67b53082074a7b76c
xpioneer <1286283280@qq.com>
2017-09-20 00:42:36 +0800

    authrize undone

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -5,20 +5,32 @@ const PROD = process.env.NODE_ENV === "production";
 export default async(ctx, next) => {
     const cur_user =  ctx.session['CUR_USER'];
     const auth_token =  ctx.session['AUTH_TOKEN'];
-    console.log(cur_user, 'cur_user')
-    if(cur_user && ctx.url.indexOf('/api/') == 0){
-        if(!PROD && ctx.query['nologin'] == 99){
-            await next();
-        }else{
-            let key = ctx.header['Authorization-User'] || ctx.header['authorization-user'] || ctx.query['Authorization-User'];
-            if(key && key.length != 64){
-                ctx.throw(401)
-            }
-            auth_token === key ? await next() : ctx.throw(401)
-        }
-    } else if(!cur_user && ctx.url.indexOf('/api/login') == 0) {
+    console.log(cur_user, auth_token, 'cur_user', ctx.session)
+    if(ctx.url.indexOf('/api/login') == 0){
         await next();
     } else {
-        await next();
+        if(cur_user && ctx.url.indexOf('/api/') == 0){
+            if(!PROD && ctx.query['nologin'] == 99){
+                await next();
+                ctx.session['CSRF_TOKEN'] = 'qwe123';
+            }else{
+                let key = ctx.header['Authorization-User'] || ctx.header['authorization-user'] || ctx.query['Authorization-User'];
+                if(key && key.length !== 64){
+                    ctx.session['CSRF_TOKEN'] = undefined;
+                    ctx.throw(401);
+                }
+                if(auth_token === key){
+                    ctx.session['CUR_USER'] = cur_user;
+                    await next();
+                    ctx.session['CSRF_TOKEN'] = 'qwe123';
+                } else {
+                    ctx.session['CSRF_TOKEN'] = undefined;
+                    ctx.throw(401);
+                }
+            }
+        }else {
+            ctx.session['CSRF_TOKEN'] = undefined;
+            ctx.throw(401);
+        }
     }
 }
