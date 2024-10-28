7f969d791a53bf0071956026dd15de5245a9704d
xpioneer <1286283280@qq.com>
2024-10-28 20:34:20 +0800

    change file

--- src/interceptors/index.ts
+++ src/interceptors/index.ts
@@ -1,10 +1,9 @@
 import { APP_INTERCEPTOR } from '@nestjs/core'
 import { ResponseInterceptor } from './response'
-import { PaginatedResponseInterceptor } from './response1'
 
 export const interceptors = [
   {
     provide: APP_INTERCEPTOR,
-    useClass: PaginatedResponseInterceptor,
+    useClass: ResponseInterceptor,
   },
 ]
--- src/interceptors/response.ts
+++ src/interceptors/response.ts
@@ -1,31 +1,45 @@
-// response.interceptor.ts
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+// paginated-response.interceptor.ts
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { ResponseData, PaginatedResponse } from '../types/global'
 
 @Injectable()
-export class ResponseInterceptor implements NestInterceptor {
+export class ResponseInterceptor<T> implements NestInterceptor<T, PaginatedResponse<T>> {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      map(data => this.formatResponse(data)),
-    );
+    return next.handle().pipe(map(async (data) => this.formatResponse(context, data)))
   }
 
-  private formatResponse(data: any) {
-    if(Array.isArray(data) && !isNaN(data[1])) {
+  private formatResponse(
+    context: ExecutionContext,
+    data: any
+  ): ResponseData<T | PaginatedResponse<T>> {
+    if (Array.isArray(data) && !isNaN(data[1])) {
+      const response = context.switchToHttp().getResponse()
+      const request = context.switchToHttp().getRequest()
+      const page = parseInt(request.query.page as string, 10) || 1
+      const pageSize = parseInt(request.query.limit as string, 10) || 10
+      const [list, total] = data
+
+      const paginatedData: PaginatedResponse<T> = {
+        data: list,
+        page,
+        pageSize,
+        total,
+        next: page < Math.ceil(total / pageSize) ? page + 1 : undefined,
+        prev: page > 1 ? page - 1 : undefined,
+      }
+
       return {
         status: 200,
         msg: 'Success',
-        data: {
-          list: data[0],
-          total: data[1]
-        }
+        data: paginatedData,
       }
     }
     return {
       status: 200,
       msg: 'Success',
       data,
-    };
+    }
   }
 }
